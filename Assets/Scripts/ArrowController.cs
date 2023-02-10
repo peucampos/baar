c9f0702318ebac6f223b9076c9bff1c7f3e8e818
peucampos <peucampos@gmail.com>
2023-02-10 10:05:51 -0300

    Particle System

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,11 @@ public class ArrowController : MonoBehaviour
 {
     private float arrowEndX = 15f;
     private float arrowSpeed = 3f;
+    public ParticleSystem explosionPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,12 +20,22 @@ public class ArrowController : MonoBehaviour
     {
         transform.Translate(Vector2.right * Time.deltaTime * arrowSpeed);
 
-        if (transform.position.x > arrowEndX)            
+        if (transform.position.x > arrowEndX)
             Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        ExplodeBalloon(collision.gameObject.transform);
         Destroy(collision.gameObject.transform.parent.gameObject);
+    }
+
+    private void ExplodeBalloon(Transform transform)
+    {
+        ParticleSystem explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = transform.position;
+        explosion.startColor = Color.red;
+        explosion.Play();
+        Destroy(explosion.gameObject, 2f);
     }
 }
