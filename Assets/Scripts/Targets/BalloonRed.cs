80b269f1d6c895277e1ffb734b4249c466e94c51
peucampos <peucampos@gmail.com>
2023-02-19 10:18:59 -0300

    Level 3

--- Assets/Scripts/Targets/BalloonRed.cs
+++ Assets/Scripts/Targets/BalloonRed.cs
@@ -6,22 +6,21 @@ public class BalloonRed : MonoBehaviour
 {
     public float baseSpeed = 1f;
     public float maxSpeed = 1f;
-    public float fallDistance = 12f;
-    public Vector3 initialPosition;
+    private float lowerBound = -7f;
+    private float upperBound = 5f;
     private float balloodSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        initialPosition = transform.position;
         balloodSpeed = Random.Range(baseSpeed, maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > initialPosition.y + fallDistance)
-            transform.position = initialPosition;
+        if (transform.position.y > upperBound)
+            transform.position = new Vector2(transform.position.x, lowerBound);
         else
             transform.Translate(balloodSpeed * Time.deltaTime * Vector2.up);
     }
