51856d20524496360a0a5e665bc70d147d4ac08a
Pedro Campos <peucampos@gmail.com>
2025-04-09 16:13:43 -0300

    Adição de duas novas cenas.
    Diversos arquivos de cena (`01-Reds.unity` a `11-AddSubOneShot.unity`) foram modificados para remover configurações de oclusão, renderização, iluminação e navegação. Várias instâncias de prefab tiveram suas propriedades ajustadas, incluindo `m_RootOrder`, `m_LocalPosition`, `maxSpeed` e `baseSpeed`. Novos objetos foram adicionados, como `GameController`, `Victory`, e `GameOver`, com suas respectivas configurações. Os arquivos `.meta` foram atualizados para refletir as mudanças nos GUIDs e na versão do formato de arquivo.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -59,17 +59,19 @@ public class LevelController : MonoBehaviour
     {
         string[] arrVictory = new string[]
         {
-            "Victory!\n\nLevel " + (buildIndex + 1) + " is a mess...", //1-2
-            "Victory!\n\nLevel " + (buildIndex + 1) + " looks easy...", //2-3
-            "Victory!\n\nLevel " + (buildIndex + 1) + " zig zags...", //3-4
-            "Victory!\n\nLevel " + (buildIndex + 1) + " is lonely...", //4-5
-            "Victory!\n\nLevel " + (buildIndex + 1) + " has three rounds...", //5-6
-            "Victory!\n\nLevel " + (buildIndex + 1) + " has intruders...", //6-7
-            "Victory!\n\nLevel " + (buildIndex + 1) + " is anoying...", //7-8
-            "Victory!\n\nLevel " + (buildIndex + 1) + ", let it go...", //8-9
-            "Victory!\n\nLevel " + (buildIndex + 1) + " is calculated...", //9-10
-            "Victory!\n\nLevel " + (buildIndex + 1) + " has options...", //10-11
-            "Victory!\n\nLevel " + (buildIndex + 1) + " is not ready...", //11-12
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is a mess...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " looks easy...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " zig zags...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is together...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is lonely...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is pointing up...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " has three rounds...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " has intruders...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is anoying...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + ", let it go...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is calculated...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " has options...", 
+            "Victory!\n\nLevel " + (buildIndex + 1) + " is not ready...", 
             
         };
 
